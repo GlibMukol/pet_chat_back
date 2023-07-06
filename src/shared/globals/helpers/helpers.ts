16e676d3fff2889f80cd48d757031fa8260637b7
mykola <gray.grifon@gmail.com>
2023-07-06 15:23:52 +0300

    create auth controller, helpers class

---
+++ src/shared/globals/helpers/helpers.ts
@@ -0,0 +1,22 @@
+export class Helpers {
+  static firstLatterUppercase(str: string) {
+    return str.toLowerCase()
+              .split(' ')
+              .map((item) => `${item.charAt(0).toUpperCase()}${item.slice(1).toLowerCase()}`)
+              .join(' ');
+  }
+
+  static lowerCase(str: string): string {
+    return str.toLowerCase();
+  }
+
+  static generateRundomIntegers(integerLength: number): number {
+    const characters = '0123456789';
+    let result  = ' ';
+    const charactersLength = characters.length;
+    for (let i = 0; i < integerLength; i++) {
+      result += characters.charAt(Math.floor(Math.random()*charactersLength));
+    }
+    return parseInt(result, 10);
+  }
+}
